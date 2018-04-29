782d1e106b9024777d2390d486970acbe0048caa
BARASOAP <ohheyitsniel@aol.com>
2018-04-29 02:21:01 -0400

    Changed so the API reads twilio information now.
    The entire thing actually works now.

--- README.md
+++ README.md
@@ -10,10 +10,13 @@ The format is as follows:
 ```
 botToken = "<Discord Bot Token>"
 directory = {
-    "<@User>": "+1<UserPhone1>", 
-    "<@User2>": "+<UserPhone2>", 
-    "<@User3>": "+<UserPhone3>"
+    "<@User1>": "+1<UserPhone1>", 
+    "<@User2>": "+1<UserPhone2>", 
+    "<@User3>": "+1<UserPhone3>"
     }
+twilioNumber = "+1<twilioNumber>"
+twilioAccountSid = "<Twilio Account SID>"
+twilioAuthToken  = "<Twilio Auth Token>"
 ```
 
 For Example:
@@ -25,6 +28,9 @@ directory = {
     "<@123456789012345678>": "+13218675309", 
     "<@999999999999999999>": "+11234567890"
     }
+twilioNumber = "+19999999999"
+twilioAccountSid = "AC1966966966996666thisisfake"
+twilioAuthToken  = "445dc5554s946421679thisisfaketoo"
 ```
 
-The Phone Number format is E.164 so Twilio can recognize it. i.e. +1 XXX - XXX - XXXX
+The Phone Number format is E.164 so Twilio can recognize it. i.e. +1 XXX-XXX-XXXX
--- SahibBot.py
+++ SahibBot.py
@@ -4,44 +4,51 @@ import discord
 import asyncio
 import time
 import keys
+from twilio.rest import Client
 
-botName      = 'Yukkuri' 
-# Eventually, we'll have Yukkuri talk more so 
+botName      = 'Yukkuri'
+# Eventually, we'll have Yukkuri talk more so
 # this will be a safemeasure so there are no infinite loops
 # of Yukkuri talking to itself
 
-directory = keys.directory 
+#Importing keys
+directory = keys.directory
 discordBotToken = keys.botToken
+
+# requires twilio account and API keys.
+twilioNumber = keys.twilioNumber
+twilioAccountSid = keys.twilioAccountSid
+twilioAuthToken = keys.twilioAuthToken
+
+# Initialize twilio and discord apis.
+twilioClient = Client(twilioAccountSid, twilioAuthToken)
 discordClient = discord.Client()
 
+#Show Console we are logged in.
 @discordClient.event
 async def on_ready():
     print('Logged in as ' + discordClient.user.name + " " + discordClient.user.id)
     print('------')
 
+
 @discordClient.event
 async def on_message(message):
     if message.content.startswith('!ping '):
         print("Message fits filter... {}: {}".format(message.author.name,message.content[6:]))
-        #await discordClient.send_message(message.channel, "DEBUG OUT: Author:{} Mentions:{} Message:{} Match:{}".format(message.author.name, message.mentions, message.content, directory[message.mentions[0].mention]))
 
         # Only process the mentions if they only mention one user. List is unreliable otherwise. Functionality will change later.
         if len(message.mentions) == 1:
             print("MENTION PASS")
             for user in directory:
-                print("{} {}".format(user, message.mentions[0].mention))
                 if user == message.mentions[0].mention:
                     await discordClient.send_message(message.channel, "DEBUG OUT: User Found {}".format(directory[user]))
+                    twilioClient.messages.create(
+                        to    = directory[user],
+                        from_ = twilioNumber,
+                        body  = "{} has pinged you in discord!".format(message.author.name),
+                    )
+                    print("Message Sent: '{} has pinged you in discord!'".format(message.author.name))
         else:
             await discordClient.send_message(message.channel, "ERR: Multiple mentions therefore list is unreliable.")
-
-
-    #if message.author.name != botName:
-    #    twilioClient.messages.create(
-    #        to    = phoneNumber,
-    #        from_ = twilioNumber,
-    #        body  = "{} - {}".format(message.author.name,message.content),
-    #    )
-    #    print("sent message: {} - {}".format(message.author.name,message.content))
 
 discordClient.run(discordBotToken)
