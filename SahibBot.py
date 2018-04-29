f1fd685fd02b8d52645c6f919b59fd1a696123c4
BARASOAP <ohheyitsniel@aol.com>
2018-04-29 15:31:13 -0400

    Adding first command for ctx
    Have to comment and rethink how we do the commands if we're going to use ctx.

--- SahibBot.py
+++ SahibBot.py
@@ -1,6 +1,7 @@
 # listen for ping on discord server (globally, all channels)
 # Sends a ping to user's phone if outside of cooldown.
 import discord
+from discord.ext import commands
 import asyncio
 import time
 import keys
@@ -22,33 +23,39 @@ twilioAuthToken = keys.twilioAuthToken
 
 # Initialize twilio and discord apis.
 twilioClient = Client(twilioAccountSid, twilioAuthToken)
-discordClient = discord.Client()
+# discordClient = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-#Show Console we are logged in.
-@discordClient.event
-async def on_ready():
-    print('Logged in as ' + discordClient.user.name + " " + discordClient.user.id)
-    print('------')
+# First test command
+@bot.command()
+async def test(ctx):
+    pass
+
+# #Show Console we are logged in.
+# @discordClient.event
+# async def on_ready():
+#     print('Logged in as ' + discordClient.user.name + " " + discordClient.user.id)
+#     print('------')
 
 
-@discordClient.event
-async def on_message(message):
-    if message.content.startswith('!ping '):
-        print("Message fits filter... {}: {}".format(message.author.name,message.content[6:]))
+# @discordClient.event
+# async def on_message(message):
+#     if message.content.startswith('!ping '):
+#         print("Message fits filter... {}: {}".format(message.author.name,message.content[6:]))
 
-        # Only process the mentions if they only mention one user. List is unreliable otherwise. Functionality will change later.
-        if len(message.mentions) == 1:
-            print("MENTION PASS")
-            for user in directory:
-                if user == message.mentions[0].mention:
-                    await discordClient.send_message(message.channel, "DEBUG OUT: User Found {}".format(directory[user]))
-                    twilioClient.messages.create(
-                        to    = directory[user],
-                        from_ = twilioNumber,
-                        body  = "{} has pinged you in discord!".format(message.author.name),
-                    )
-                    print("Message Sent: '{} has pinged you in discord!'".format(message.author.name))
-        else:
-            await discordClient.send_message(message.channel, "ERR: Multiple mentions therefore list is unreliable.")
+#         # Only process the mentions if they only mention one user. List is unreliable otherwise. Functionality will change later.
+#         if len(message.mentions) == 1:
+#             print("MENTION PASS")
+#             for user in directory:
+#                 if user == message.mentions[0].mention:
+#                     await discordClient.send_message(message.channel, "DEBUG OUT: User Found {}".format(directory[user]))
+#                     twilioClient.messages.create(
+#                         to    = directory[user],
+#                         from_ = twilioNumber,
+#                         body  = "{} has pinged you in discord!".format(message.author.name),
+#                     )
+#                     print("Message Sent: '{} has pinged you in discord!'".format(message.author.name))
+#         else:
+#             await discordClient.send_message(message.channel, "ERR: Multiple mentions therefore list is unreliable.")
 
-discordClient.run(discordBotToken)
+# discordClient.run(discordBotToken)
