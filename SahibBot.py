2a24416f472a26699291d06dbe7744a517de96c8
BARASOAP <ohheyitsniel@aol.com>
2018-04-30 04:17:00 -0400

    Cleanup and added error handling for ping
    removed time import because we will use the extended commands for that. Also ctx naturally handles multiple mentions so we will just make it so it only takes one. Changed format so it returns a mention instead of an id.

--- SahibBot.py
+++ SahibBot.py
@@ -3,7 +3,6 @@
 import discord
 from discord.ext import commands
 import asyncio
-import time
 import keys
 from twilio.rest import Client
 
@@ -23,20 +22,19 @@ discordBotToken = keys.botToken
 
 # Initialize twilio and discord apis.
 # twilioClient = Client(twilioAccountSid, twilioAuthToken)
-# discordClient = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
 # First test command
 @bot.command(pass_context = True)
-async def ping(ctx, *, member : discord.Member = None):
+async def ping(ctx, member : discord.Member = None):
     if member is None:
         await bot.say(ctx.message.author.mention + ": A person is needs to be included when pinging someone!")
     else:
     # if len(message.mentions) == 1:
         print("MENTION PASS")
-        print("User " + ctx.message.author.name + "(" + ctx.message.author.id + ")" + " is mentioning " + member.name + "(" + member.id + ")")
+        # print("User " + ctx.message.author.name + "(" + ctx.message.author.id + ")" + " is mentioning " + member.name + "(" + member.id + ")")
         for user in directory:
-            if user == member.id:
+            if user == member.mention:
                 await bot.say("DEBUG OUT: User Found {}".format(directory[user]))
                 # twilioClient.messages.create(
                 #     to    = directory[user],
@@ -44,33 +42,22 @@ async def ping(ctx, *, member : discord.Member = None):
                 #     body  = "{} has pinged you in discord!".format(message.author.name),
                 # )
                 print("Message Sent: '{} has pinged you in discord!'".format(ctx.message.author.name))
-                await bot.say(ctx.message.author.mention + ": Successfully pinged " + member.name + "(" + member.id + ")" + " at {}".format(directory[user]))
+                await bot.say(ctx.message.author.mention + ": Successfully pinged " + member.mention + "(" + member.id + ")" + " at {}".format(directory[user]))
 
-#Show Console we are logged in.
+# Show Console we are logged in.
 @bot.event
 async def on_ready():
     print('Logged in as ' + bot.user.name + " " + bot.user.id)
     print('------')
 
-
-# @discordClient.event
-# async def on_message(message):
-#     if message.content.startswith('!ping '):
-#         print("Message fits filter... {}: {}".format(message.author.name,message.content[6:]))
-
-#         # Only process the mentions if they only mention one user. List is unreliable otherwise. Functionality will change later.
-#         if len(message.mentions) == 1:
-#             print("MENTION PASS")
-#             for user in directory:
-#                 if user == message.mentions[0].mention:
-#                     await discordClient.send_message(message.channel, "DEBUG OUT: User Found {}".format(directory[user]))
-#                     twilioClient.messages.create(
-#                         to    = directory[user],
-#                         from_ = twilioNumber,
-#                         body  = "{} has pinged you in discord!".format(message.author.name),
-#                     )
-#                     print("Message Sent: '{} has pinged you in discord!'".format(message.author.name))
-#         else:
-#             await discordClient.send_message(message.channel, "ERR: Multiple mentions therefore list is unreliable.")
+# Commands has its own way of handling errors. 
+@ping.error
+async def ping_error(ctx, error):
+    print(dir(error))
+    print(commands.BadArgument)
+    if isinstance(error, commands.errors.BadArgument):
+        await bot.say("ERROR HOLY CRAP")
+    else:
+        print(type(error), error)
 
 bot.run(discordBotToken)
