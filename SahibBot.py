a1abcbf50d1005db2c2714e98e10742d5305ce18
BARASOAP <ohheyitsniel@aol.com>
2018-05-01 09:55:49 -0400

    Cooldown now functional.
    Cooldown now functions as it is supposed to. Error handling provides good information to console. Removed debugging output and removed tsundere from the bot.

--- SahibBot.py
+++ SahibBot.py
@@ -11,60 +11,50 @@ botName      = 'Yukkuri'
 # this will be a safemeasure so there are no infinite loops
 # of Yukkuri talking to itself
 
-#Importing keys
 directory = keys.directory
 discordBotToken = keys.botToken
 
-# requires twilio account and API keys.
 # twilioNumber = keys.twilioNumber
 # twilioAccountSid = keys.twilioAccountSid
 # twilioAuthToken = keys.twilioAuthToken
 
-# Initialize twilio and discord apis.
 # twilioClient = Client(twilioAccountSid, twilioAuthToken)
 bot = commands.Bot(command_prefix='!')
 
-# First test command
+@commands.cooldown(1, 60 * 30, commands.cooldowns.BucketType.user)
 @bot.command(pass_context = True)
 async def ping(ctx, member : discord.Member = None):
-    if member is None:
-        await bot.say(ctx.message.author.mention + 
-        	": A person is needs to be included when pinging someone!")
-    else:
-    # if len(message.mentions) == 1:
-        print("MENTION PASS")
-        # print("User " + ctx.message.author.name + "(" + ctx.message.author.id + ")" + " is mentioning " + member.name + "(" + member.id + ")")
-        for user in directory:
-            if user == member.mention:
-                await bot.say("DEBUG OUT: User Found {}".format(directory[user]))
-                # twilioClient.messages.create(
-                #     to    = directory[user],
-                #     from_ = twilioNumber,
-                #     body  = "{} has pinged you in discord!".format(message.author.name),
-                # )
-                print("Message Sent: '{} has pinged you in discord!'".format(
-                	ctx.message.author.name
-                	))
-                await bot.say(
-                	ctx.message.author.mention + 
-                	": Successfully pinged " + member.mention + 
-                	"(" + member.id + ")" + " at {}".format(directory[user])
-                	)
+    for user in directory:
+        if user == member.mention:
+            # twilioClient.messages.create(
+            #     to    = directory[user],
+            #     from_ = twilioNumber,
+            #     body  = "{} has pinged you in discord!".format(message.author.name),
+            # )
+            print("Message Sent: '{} has pinged you in discord!'".format(
+                ctx.message.author.name
+                ))
+            await bot.say("{}: Ping sent to {} at {}!".format(ctx.message.author.mention, 
+                member.mention, directory[user]
+                ))
 
-# Show Console we are logged in.
 @bot.event
 async def on_ready():
     print('Logged in as ' + bot.user.name + " " + bot.user.id)
     print('------')
 
-# Commands has its own way of handling errors.
 @ping.error
 async def ping_error(error, ctx):
     if isinstance(error, commands.errors.BadArgument):
-        await bot.say("{}: Really? How about you @Mention someone buddy.".format(
+        await bot.say("{}: Please mention someone in your command.".format(
         	ctx.message.author.mention
         	))
+    elif isinstance(error, commands.errors.CommandOnCooldown):
+        m, s = divmod(error.retry_after, 60)
+        await bot.say("{}: Cooldown for your user is active for {:.0f} minutes and {:.0f} seconds.".format(
+            ctx.message.author.mention, m, s
+        ))
     else:
-    	print("Unhandled error for ping module: {} | {} ".format(error, ctx))
+    	print("Unhandled error for ping module: {}".format(error))
 
 bot.run(discordBotToken)
