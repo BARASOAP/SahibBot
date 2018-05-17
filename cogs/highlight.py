e7e29c779cfc86bbbb5e02b1cbbea648568c811b
BARASOAP <ohheyitsniel@aol.com>
2018-05-16 23:42:31 -0400

    All functionality complete.
    Highlight cog now works as expected.

--- cogs/highlight.py
+++ cogs/highlight.py
@@ -8,10 +8,28 @@ class Highlight:
     def __init__(self, bot):
         self.bot = bot
         self.highlight_channel = '446287440920182784'
+        self.message_bank = {}
+
+    def create_embed(self, react, user):
+        payload = discord.Embed(
+            description = f'{react.message.clean_content}',
+            color = discord.Color.gold(),
+            timestamp = react.message.timestamp
+        )
+        payload.set_author(
+            name = react.message.author.display_name,
+            icon_url = react.message.author.avatar_url
+        )
+        return payload
+
+    def create_content(self, react):
+        reactions = ''
+        for reaction in react.message.reactions:
+            reactions = reactions + f'{reaction.emoji}{reaction.count} '
+        return reactions
 
     @commands.command(pass_context = True)
     async def hlstatus(self, ctx):
-        # await self.bot.say(f'{__name__} is active')
         await self.bot.send_message(ctx.message.channel, 
             embed = discord.Embed(
                 title = 'Highlight Status', 
@@ -21,24 +39,30 @@ class Highlight:
         )
 
     async def on_reaction_add(self, react, user):
-
-        message_embed = discord.Embed(
-            description = f'{react.message.clean_content}',
-            color = discord.Color.gold()
-        )
-        message_embed.set_author(
-                name = react.message.author.display_name,
-                icon_url = react.message.author.avatar_url
+        if react.message in self.message_bank:
+            await self.bot.edit_message(self.message_bank[react.message],
+                new_content = self.create_content(react),
+                embed = self.create_embed(react, user)
             )
+        else:
+            new_message = await self.bot.send_message(self.bot.get_channel(self.highlight_channel),
+                content = self.create_content(react),
+                embed = self.create_embed(react, user)
+            )
+            self.message_bank.update({react.message: new_message})
 
-        reactions = ''
-        for reaction in react.message.reactions:
-            reactions = reactions + f'{reaction.emoji}{reaction.count} '
-
-        await self.bot.send_message(self.bot.get_channel(self.highlight_channel),
-            content = reactions,
-            embed = message_embed
-        )
+    async def on_reaction_remove(self, react, user):
+        if len(react.message.reactions) == 0:
+            if react.message in self.message_bank:
+                await self.bot.delete_message(self.message_bank[react.message])
+                del self.message_bank[react.message]
+            else:
+                pass
+        else:
+            await self.bot.edit_message(self.message_bank[react.message],
+                new_content = self.create_content(react),
+                embed = self.create_embed(react, user)
+            )
 
 def setup(bot):
     bot.add_cog(Highlight(bot))
